9cacbbe9c5fe96a5c6f9d3704538140010cb04b0
freddy.liu <freddy.liu@positivegrid.com>
2023-10-28 12:22:12 +0800

    chore: init

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express, { Express, Request, Response } from "express";
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running in http://localhost:${PORT}`);
+});
